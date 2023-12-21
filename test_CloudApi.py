223d3995bc4be275cc3ac53580d4af30c5e12b9a
Sergei <sergei@MacBook-Air-Sergei.local>
2023-12-21 18:19:17 +0300

    Run test

--- test_CloudApi.py
+++ test_CloudApi.py
@@ -4,6 +4,8 @@ from CloudApi import app
 allow_redirects=False
 client = TestClient(app)
 
+
+# Тест для api и проверка результата модели
 def test_read_CloudApi():
     response = client.post("/answer/", json={"question": "Вопрос", "search_topic": "Контекст"})
     assert response.status_code == 200
